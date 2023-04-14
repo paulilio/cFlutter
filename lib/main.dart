bb58166fd014b69c6508c2658efdbd47a9d13614
paulilio <paulilio.ferreira@gmail.com>
2023-04-14 16:44:16 -0300

    current format

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,46 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: _transaction.map((tr) {
               return Card(
-                child: Text(tr.title),
-              );
+                  child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 15,
+                      vertical: 10,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 225, 123, 243),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        tr.date.toString(),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ));
             }).toList(),
           )
         ],
