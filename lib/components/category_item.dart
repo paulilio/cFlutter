f59f988578291194c4713f3e3e62cf095c40a253
paulilio <paulilio.ferreira@gmail.com>
2023-04-19 08:19:38 -0300

    categories screen

---
+++ lib/components/category_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem(this.category, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Text(category.title),
+    );
+  }
+}
