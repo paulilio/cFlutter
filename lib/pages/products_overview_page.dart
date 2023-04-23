dea9d3f8c7c85cfd97a6cf5837fe29acb46f190a
paulilio <paulilio.ferreira@gmail.com>
2023-04-23 20:47:52 -0300

    product manager function

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/components/app_drawer.dart';
-import 'package:shop/components/badge.dart';
+import 'package:shop/components/Badge.dart';
 import 'package:shop/components/product_grid.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/utils/app_routes.dart';
