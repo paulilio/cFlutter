88a02dc206f99b9f745abb536b4b00ec074fea89
paulilio <paulilio.ferreira@gmail.com>
2023-04-25 20:04:18 -0300

    fnc save order with firebase

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const productBaseUrl =
       'https://cflutter-shop-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://cflutter-shop-default-rtdb.firebaseio.com/orders';
 }
