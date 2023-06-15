fc9b498769dde8e9782c2b2bf512f41ba64298a6
Paulilio <paulilio.ferreira@gmail.com>
2023-06-15 16:16:27 -0300

    Routes: Initializing with detault route mechanism (Navigator push/pop)

---
+++ lib/mainRotas.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Tela1());
+  }
+}
+
+class Tela1 extends StatelessWidget {
+  const Tela1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('Tela 1')),
+            body: Center(
+              child: ElevatedButton(
+                child: const Text('Ir para Tela 2'),
+                onPressed: () {
+                  print('Clicou no botão da tela 1');
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return Tela2();
+                  }));
+                },
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+class Tela2 extends StatelessWidget {
+  const Tela2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Tela 2'),
+            backgroundColor: Colors.red,
+          ),
+          body: Center(
+              child: ElevatedButton(
+                  child: const Text('Retornar a Tela 1'),
+                  onPressed: () {
+                    print('Clicou no botão da tela 2');
+                    Navigator.pop(context);
+                  })),
+        ),
+      ),
+    );
+  }
+}
